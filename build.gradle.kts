754d1b6abe902c435ae87f2ccc3aedd145091fe2
T14D3 <73843330+T14D3@users.noreply.github.com>
2025-06-25 19:32:38 +0200

    Small fixes, deps shading, etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id ("java")
     id("xyz.jpenilla.run-paper") version "2.3.1"
+    id("com.gradleup.shadow") version "9.0.0-beta8"
 }
 
 group = "de.t14d3"
@@ -20,7 +21,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.6-R0.1-SNAPSHOT")
-    implementation("dev.jorel:commandapi-bukkit-shade-mojang-mapped:9.7.0")
+    implementation("dev.jorel:commandapi-bukkit-shade:10.1.0")
 }
 
 tasks {
