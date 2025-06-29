1037768608659993a76f93861cb9f53932f9ce24
T14D3 <73843330+T14D3@users.noreply.github.com>
2025-06-29 09:36:45 +0200

    Fix home system (wasn't broken just extraordinarily stupid)

--- src/main/java/de/t14d3/core/CommandManager.java
+++ src/main/java/de/t14d3/core/CommandManager.java
@@ -1,19 +1,18 @@
 package de.t14d3.core;
 
 import de.t14d3.core.commands.*;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-
-import java.nio.file.Files;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.NamespacedKey;
 
 public class CommandManager {
     private final Main plugin;
-    private final FileConfiguration homes;
 
     public CommandManager(Main plugin) {
         this.plugin = plugin;
+
         new AnvilCommand();
         new BackCommand();
         new BroadcastCommand();
@@ -38,56 +37,28 @@ public class CommandManager {
         new TeamChatCommand();
         new UInfoCommand();
         new VanishCommand();
-
-        FileConfiguration homes = new YamlConfiguration();
-        try {
-            homes.load(plugin.getDataFolder().toPath().resolve("homes.yml").toFile());
-        } catch (Exception e) {
-            plugin.getLogger().warning("Failed to load homes.yml: " + e.getMessage());
-            try {
-                Files.createFile(plugin.getDataFolder().toPath().resolve("homes.yml"));
-            } catch (Exception e1) {
-                plugin.getLogger().warning("Failed to create homes.yml: " + e1.getMessage());
-            }
-        }
-        this.homes = homes;
-
-    }
-
-    public FileConfiguration getHomes() {
-        return homes;
     }
 
     public void setHomeLocation(Player player, Location location) {
-        homes.set("homes." + player.getUniqueId() + ".x", location.getX());
-        homes.set("homes." + player.getUniqueId() + ".y", location.getY());
-        homes.set("homes." + player.getUniqueId() + ".z", location.getZ());
-        homes.set("homes." + player.getUniqueId() + ".yaw", location.getYaw());
-        homes.set("homes." + player.getUniqueId() + ".pitch", location.getPitch());
-        homes.set("homes." + player.getUniqueId() + ".world", location.getWorld().getName());
-        saveHomes();
+        NamespacedKey key = new NamespacedKey(plugin, "home");
+        player.getPersistentDataContainer().set(key, PersistentDataType.STRING, locationToString(location));
     }
 
     public Location getHomeLocation(Player player) {
-        if (!homes.isSet("homes." + player.getUniqueId())) {
+        NamespacedKey key = new NamespacedKey(plugin, "home");
+        String homeData = player.getPersistentDataContainer().get(key, PersistentDataType.STRING);
+        if (homeData == null) {
             return null;
         }
-        double x = homes.getDouble("homes." + player.getUniqueId() + ".x");
-        double y = homes.getDouble("homes." + player.getUniqueId() + ".y");
-        double z = homes.getDouble("homes." + player.getUniqueId() + ".z");
-        float yaw = (float) homes.getDouble("homes." + player.getUniqueId() + ".yaw");
-        float pitch = (float) homes.getDouble("homes." + player.getUniqueId() + ".pitch");
-        String world = homes.getString("homes." + player.getUniqueId() + ".world");
-        return new Location(Main.getInstance().getServer().getWorld(world), x, y, z, yaw, pitch);
+        return stringToLocation(homeData);
     }
 
-    public void saveHomes() {
-        try {
-            homes.save(plugin.getDataFolder().toPath().resolve("homes.yml").toFile());
-        } catch (Exception e) {
-            plugin.getLogger().warning("Failed to save homes.yml: " + e.getMessage());
-        }
+    private String locationToString(Location location) {
+        return location.getWorld().getName() + "," + location.getX() + "," + location.getY() + "," + location.getZ() + "," + location.getYaw() + "," + location.getPitch();
     }
 
-
+    private Location stringToLocation(String data) {
+        String[] parts = data.split(",");
+        return new Location(Bukkit.getWorld(parts[0]), Double.parseDouble(parts[1]), Double.parseDouble(parts[2]), Double.parseDouble(parts[3]), Float.parseFloat(parts[4]), Float.parseFloat(parts[5]));
+    }
 }
